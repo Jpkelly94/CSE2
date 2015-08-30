99085fc0ad3fee39bfcaafafb693ca407bf5fad9
John_Kelly <jpk217@lehigh.edu>
2015-08-30 02:58:30 +0000

    updated autobio

---
+++ hw01/Autobio.java
@@ -0,0 +1,22 @@
+//hw01: Autobiography
+//John Kelly
+//August 29th, 2015
+
+public class Autobio {
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("^  ^  ^  ^  ^  ^");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-j--p--k--2--1--7->");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println(" v  v  v  v  v  v");
+        System.out.println("I am a junior looking to minor in CS. I have experience in Python, and want to learn how to develop a website and create trading algorithms.");
+        
+    }
+}
+
